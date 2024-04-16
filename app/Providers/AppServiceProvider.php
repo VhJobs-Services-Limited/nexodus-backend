20eec789466f04e5e6135eef8506cf1ee0483e6d
CodeAddictx10 <onadeji.samuel@yahoo.com>
2024-04-16 22:41:51 +0100

    feat: update packages

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,8 +19,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-
-        //You can update the token expiry date here
-        Passport::personalAccessTokensExpireIn(now()->addMonths(6));
     }
 }
