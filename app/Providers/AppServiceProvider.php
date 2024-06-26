ab4714f78329b2e516b66d2f325b6499400348fe
CodeAddictx10 <onadeji.samuel@yahoo.com>
2024-06-26 12:37:46 +0100

    feat: add model strictness

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,5 +20,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Model::shouldBeStrict(config('app.env') == 'local');
+
+        Model::handleMissingAttributeViolationUsing(function (Model $model, string $column) {
+            $class = $model::class;
+
+            info("Attempted to read missing column: [{$column}] on model [{$class}].");
+        });
+
+
+        Model::handleLazyLoadingViolationUsing(function (Model $model, string $relation) {
+            $class = $model::class;
+
+            info("Attempted to lazy load [{$relation}] on model [{$class}].");
+        });
+
+        Model::handleDiscardedAttributeViolationUsing(function (Model $model, string $column) {
+            $class = $model::class;
+
+            info("Attempted to add disacrd column: [{$column}] on model [{$class}].");
+        });
+
     }
 }
