5473afa1edbd1b511537abe27dc0aaaef08c8e9e
CodeAddictx10 <onadeji.samuel@yahoo.com>
2025-06-23 19:03:22 +0100

    feat: add scheduled command for queue processing
    - Introduced a new scheduled command to run 'queue:work' for payments, notifications, and default queues every minute.
    - Ensured the command does not overlap with previous executions.

--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+Schedule::command('queue:work --queue=payments,notifications,default')
+    ->everyMinute()
+    ->withoutOverlapping();
